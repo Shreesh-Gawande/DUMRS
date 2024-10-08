c683872974752a0e4304de315ed94356dabe0451
Rahul S <rahulsreekumar03@gmail.com>
2024-10-08 21:08:47 +0530

    set up server file

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
 const express = require('express');
 require('dotenv').config();
-
+const cookie_parser = require('cookie-parser');
 
 const PORT = process.env.PORT || 4000;
+const CLIENT_URL = process.env.CLIENT_URL;
 
 const app = express();
 
 // middlewares
 
-app.use(express.json());  // To parse JSON bodies
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+app.use(cors({
+    origin: CLIENT_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+}));
+app.use(cookie_parser());   
 app.use(morgan('dev'));   // HTTP request logging
 
 // routes
