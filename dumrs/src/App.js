56d712dca3524134d3dbb9222306a0128c824ce3
rishi-2811 <mishrarishi2911@gmail.com>
2024-11-10 16:01:03 +0530

    added profile,add patient

--- dumrs/src/App.js
+++ dumrs/src/App.js
@@ -4,6 +4,8 @@ import { DoctorLogin } from "./pages/doctorlogin";
 import {DashboardPage} from "./pages/dashboard";
 import MedicalProfile from "./pages/medicalInfo";
 import AddMedicalRecord from "./pages/addrecord";
+import PatientRegistrationForm from "./pages/addpatient";
+import Profile from "./pages/profile";
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
           <Route path="/dashboard" element={<DashboardPage/>}/>
           <Route path="/medical-info" element={<MedicalProfile/>}/>
           <Route path="/add-record" element={<AddMedicalRecord/>}/>
+          <Route path="/add-patient" element={<PatientRegistrationForm/>}/>
+          <Route path="/profile" element={<Profile/>}/>
        </Routes>
     </BrowserRouter>
   );
