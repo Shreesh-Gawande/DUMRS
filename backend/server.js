607c47a0c6ac53cad2d28ac9cbd57896c41465ef
Shreesh Gawande <shreeshgawande10@gmail.com>
2024-11-10 15:59:51 +0530

    Added authentication

--- backend/server.js
+++ backend/server.js
@@ -1,48 +1,38 @@
+// server.js
 const express = require("express");
 require("dotenv").config();
-const cookie_parser = require("cookie-parser");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const morgan = require("morgan");
-const authenticationRoutes=require("./routes/authenticationRoutes")
+const mongoose = require("mongoose");
+const authenticationRoutes = require("./routes/authenticationRoutes");
 
 const PORT = process.env.PORT || 4000;
 const CLIENT_URL = process.env.CLIENT_URL;
 const app = express();
-app.use(express.json());
-app.use("/auth",authenticationRoutes);
-
-
-/* const auth = require("./middlewares/auth");
-
-const patientRoutes = require("./routes/patientRoutes");
-const hospitalRoutes = require("./routes/hospitalRoutes");
-const authenticationRoutes = require("./routes/authenticationRoutes");
-const authorizedRoutes = require("./routes/authorizedRoutes");
-
-
 
 // Middlewares
+app.use(cors());
 
+app.use(express.json());  // Middleware for parsing JSON request bodies
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: CLIENT_URL,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+app.use(cookieParser());
+app.use(morgan("dev"));  // Logging requests
+
+// MongoDB Connection
+mongoose.connect("mongodb+srv://ShreeshGawande:Shreesh10@cluster0.o3pndei.mongodb.net/")
+  .then(() => {
+    console.log("MongoDB Connected");
   })
-);
-app.use(cookie_parser());
-app.use(morgan("dev")); // HTTP request logging
+  .catch((error) => {
+    console.error("MongoDB Connection Error:", error);
+  });
 
 // Routes
-app.use("/api/v1/patient", patientRoutes);
-app.use("/api/v1/auth", authenticationRoutes);
-app.use("/api/v1", auth, authorizedRoutes); // Protected routes, requires authorization.
-app.use("/api/v1/hospital", hospitalRoutes);
- */
-// Server
+app.use("/auth/", authenticationRoutes); // Prefixing all routes with /auth
 
+
+// Server
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
